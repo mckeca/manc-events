214c9f1dd87f3be90d832a144f21a5643ec6a325
mckeca <43346177+mckeca@users.noreply.github.com>
2020-01-07 15:08:20 +0000

    finished and it's awesome whut whut

--- manc_events/src/Components/EventCard.js
+++ manc_events/src/Components/EventCard.js
@@ -1,18 +1,23 @@
 import React from 'react';
 
 const EventCard = ({ event }) => {
-	return (
-		<li key={event.id} className="event-card">
-			<img src={event.image} alt="band pic"></img>
-			<h2>{event.name}</h2>
-			<p>
-				{event.eventType}/{event.genre}
-			</p>
-			<p>Date: {event.date}</p>
-			<p>Time: {event.time}</p>
-			<a href={event.url}>Event Page</a>
-		</li>
-	);
+  return (
+    <li
+      key={event.id}
+      className="event-card"
+      onClick={() => {
+        document.location.href = event.url;
+      }}
+    >
+      <img src={event.image} alt="band pic"></img>
+      <h2>{event.name}</h2>
+      <p>
+        {event.eventType}/{event.genre}
+      </p>
+      <p>Date: {event.date}</p>
+      <p>Time: {event.time}</p>
+    </li>
+  );
 };
 
 export default EventCard;
--- manc_events/src/Components/Search.js
+++ manc_events/src/Components/Search.js
@@ -1,36 +1,37 @@
 import React, { Component } from 'react';
 
 class Search extends Component {
-	state = {
-		searchText: ''
-	};
-	render() {
-		return (
-			<form
-				id="search-form"
-				onSubmit={event => {
-					this.props.handleSearch(this.state.searchText);
-					event.preventDefault();
-				}}
-			>
-				<label>
-					<input
-						type="text"
-						onChange={this.handleChange}
-						value={this.state.searchText}
-					></input>
-					<button>Search</button>
-				</label>
-			</form>
-		);
-	}
+  state = {
+    searchText: ''
+  };
+  render() {
+    return (
+      <form
+        id="search-form"
+        onSubmit={event => {
+          this.props.handleSearch(this.state.searchText);
+          event.preventDefault();
+        }}
+      >
+        <label>
+          <input
+            type="text"
+            onChange={this.handleChange}
+            value={this.state.searchText}
+            id="search-box"
+          ></input>
+          <button id="search-button">Search</button>
+        </label>
+      </form>
+    );
+  }
 
-	handleChange = event => {
-		const { value } = event.target;
-		this.setState({
-			searchText: value
-		});
-	};
+  handleChange = event => {
+    const { value } = event.target;
+    this.setState({
+      searchText: value
+    });
+  };
 }
 
 export default Search;
