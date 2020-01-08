29cb6be622b45294b4762ff5d8f7e6ec31f1a66e
douglashellowell <doog99@gmail.com>
2020-01-08 14:16:51 +0000

    styling added to cards

--- manc_events/src/Components/Map.js
+++ manc_events/src/Components/Map.js
@@ -2,49 +2,56 @@ import React, { Component } from 'react';
 import { Map as LeafletMap, TileLayer, Marker, Popup } from 'react-leaflet';
 
 class Map extends Component {
-  state = {
-    lat: 53.480759,
-    lng: -2.242631,
-    zoom: 13
-  };
+	state = {
+		lat: 53.480759,
+		lng: -2.242631,
+		zoom: 13
+	};
 
-  render() {
-    const position = [this.state.lat, this.state.lng];
-    const venues = {};
-    this.props.events.forEach(event => {
-      if (!Object.keys(venues).some(venue => venue === event.venue)) {
-        venues[event.venue] = {
-          eventCount: 1,
-          events: [event.name],
-          location: [event.lat, event.long]
-        };
-      } else {
-        venues[event.venue].eventCount++;
-        venues[event.venue].events.push(event.name);
-      }
-    });
-    console.log(venues);
-    return (
-      <LeafletMap center={position} zoom={this.state.zoom}>
-        <TileLayer
-          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.osm.org/{z}/{x}/{y}.png"
-        />
-        {Object.keys(venues).map(venue => {
-          return (
-            <Marker position={venues[venue].location}>
-              <Popup>
-                <h3>{venue}</h3>
-                Events: {venues[venue].eventCount}
-                <br />
-                {venues[venue].events.join(', ')}
-              </Popup>
-            </Marker>
-          );
-        })}
-      </LeafletMap>
-    );
-  }
+	render() {
+		const position = [this.state.lat, this.state.lng];
+		const venues = {};
+		this.props.events.forEach(event => {
+			if (!Object.keys(venues).some(venue => venue === event.venue)) {
+				venues[event.venue] = {
+					eventCount: 1,
+					events: [[event.url, event.name]],
+					location: [event.lat, event.long]
+				};
+			} else {
+				venues[event.venue].eventCount++;
+				venues[event.venue].events.push([event.url, event.name]);
+			}
+		});
+		// console.log(venues);
+		return (
+			<LeafletMap center={position} zoom={this.state.zoom}>
+				<TileLayer
+					attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+					url="https://{s}.tile.osm.org/{z}/{x}/{y}.png"
+				/>
+				{Object.keys(venues).map(venue => {
+					return (
+						<Marker position={venues[venue].location}>
+							<Popup>
+								<h3>{venue}</h3>
+								Events: {venues[venue].eventCount}
+								<br />
+								{venues[venue].events.map(event => {
+									return (
+										<span>
+											<a href={event[0]}>{event[1]}</a>
+											<br />
+										</span>
+									);
+								})}
+							</Popup>
+						</Marker>
+					);
+				})}
+			</LeafletMap>
+		);
+	}
 }
 
 export default Map;
--- manc_events/src/Components/utils/utils.js
+++ manc_events/src/Components/utils/utils.js
@@ -1,19 +1,20 @@
 const util = events => {
-  return events.map(event => {
-    return {
-      name: event.name,
-      id: event.id,
-      url: event.url,
-      image: event.images[0].url,
-      date: event.dates.start.localDate,
-      time: event.dates.start.localTime,
-      eventType: event.classifications[0].segment.name,
-      genre: event.classifications[0].genre.name,
-      long: event._embedded.venues[0].location.longitude,
-      lat: event._embedded.venues[0].location.latitude,
-      venue: event._embedded.venues[0].name
-    };
-  });
+	return events.map(event => {
+		return {
+			name: event.name,
+			id: event.id,
+			url: event.url,
+			image: event.images[0].url,
+			date: event.dates.start.localDate,
+			time: event.dates.start.localTime,
+			eventType: event.classifications[0].segment.name,
+			genre: event.classifications[0].genre.name,
+			long: event._embedded.venues[0].location.longitude,
+			lat: event._embedded.venues[0].location.latitude,
+			venue: event._embedded.venues[0].name,
+			price: event.priceRanges[0]
+		};
+	});
 };
 
 export default util;
