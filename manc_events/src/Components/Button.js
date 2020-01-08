fec4f824ac571be24f4773fff3d0df7dd04de00c
douglashellowell <doog99@gmail.com>
2020-01-08 17:04:11 +0000

    added button highlighting and local storage eventType memory

--- manc_events/src/Components/Button.js
+++ manc_events/src/Components/Button.js
@@ -2,16 +2,45 @@ import React from 'react';
 
 const eventType = ['All', 'Music', 'Dance', 'Theatre', 'Comedy'];
 
+const deselectBtns = () => {
+	const panel = document.getElementById('button-panel');
+	const savedStorage = localStorage.getItem('currentEventType');
+	console.log(savedStorage.slice(1, -1), '<<<<<<');
+	for (let i = 0; i < panel.children.length; i++) {
+		if (panel.children[i].innerHTML !== savedStorage.slice(1, -1)) {
+			panel.children[i].className = 'btn-unselected';
+			panel.children[i].removeAttribute('id');
+		} else {
+			panel.children[i].removeAttribute('class');
+			panel.children[i].id = 'btn-selected';
+		}
+	}
+};
+
 const Button = ({ fetchEvents }) => {
 	return eventType.map(event => {
 		if (event === 'All')
 			return (
-				<button onClick={() => fetchEvents('')} key={event}>
+				<button
+					onClick={clickEvent => {
+						fetchEvents('', '', deselectBtns);
+						clickEvent.target.removeAttribute('class');
+						clickEvent.target.id = 'btn-selected';
+					}}
+					id="btn-selected"
+					key={event}
+				>
 					{event}
 				</button>
 			);
 		return (
-			<button onClick={() => fetchEvents(event)} key={event}>
+			<button
+				onClick={clickEvent => {
+					fetchEvents('', event, deselectBtns);
+				}}
+				className="btn-unselected"
+				key={event}
+			>
 				{event}
 			</button>
 		);
--- manc_events/src/Components/List.js
+++ manc_events/src/Components/List.js
@@ -5,80 +5,86 @@ import Button from './Button';
 import Search from './Search';
 import Map from './Map';
 import Loading from './Loading';
+import axios from 'axios';
 
 class List extends Component {
-  state = {
-    events: [],
-    isLoading: true
-  };
+	state = {
+		events: [],
+		isLoading: true,
+		currentEventType: ''
+	};
 
-  componentDidMount = () => {
-    fetch(
-      `https://app.ticketmaster.com/discovery/v2/events.json?classificationName=music&countryCode=GB&city=manchester&apikey=MPEjJbqNGhQLZI0ouzQ1GCmBLYxGHsqB`
-    )
-      .then(response => {
-        return response.json();
-      })
-      .then(events => {
-        this.setState({
-          events: util(events._embedded.events),
-          isLoading: false
-        });
-      });
-  };
+	componentDidMount = () => {
+		const savedEventType = localStorage.getItem('currentEventType');
+		this.fetchEvents('', savedEventType !== '""' ? savedEventType : '');
+	};
 
-  render() {
-    return (
-      <div id="list-container">
-        <Map events={this.state.events} />
-        <section id="button-panel">
-          <Button fetchEvents={this.fetchEvents} />
-        </section>
-        <Search
-          handleSearch={this.handleSearch}
-          startLoading={this.startLoading}
-        />
-        <ul id="list-ul">
-          {!this.state.isLoading ? (
-            this.state.events.map(event => {
-              return <EventCard event={event} key={event.id} />;
-            })
-          ) : (
-            <Loading />
-          )}
-        </ul>
-      </div>
-    );
-  }
+	render() {
+		const { isLoading, events } = this.state;
+		return (
+			<div id="list-container">
+				<Map events={events} />
+				<section id="button-panel">
+					<Button fetchEvents={this.fetchEvents} />
+				</section>
+				<Search
+					handleSearch={this.handleSearch}
+					startLoading={this.startLoading}
+				/>
+				<ul id="list-ul">
+					{!isLoading ? (
+						events.length ? (
+							events.map(event => {
+								return <EventCard event={event} key={event.id} />;
+							})
+						) : (
+							<p>no events found :(</p>
+						)
+					) : (
+						<Loading />
+					)}
+				</ul>
+			</div>
+		);
+	}
 
-  fetchEvents = (keyword = '', eventType) => {
-    if (keyword) {
-      keyword = 'keyword=' + keyword + '&';
-    }
-    fetch(
-      `https://app.ticketmaster.com/discovery/v2/events.json?${keyword}classificationName=${eventType}&countryCode=GB&city=manchester&apikey=MPEjJbqNGhQLZI0ouzQ1GCmBLYxGHsqB`
-    )
-      .then(response => {
-        return response.json();
-      })
-      .then(events => {
-        if (!events.page.totalElements)
-          this.setState({ events: [], isLoading: false });
-        else
-          this.setState({
-            events: util(events._embedded.events),
-            isLoading: false
-          });
-      });
-  };
+	fetchEvents = (keyword = '', eventType = '', callback) => {
+		let keywordQuery = '';
+		let eventTypeQuery = '';
+		if (keyword) keywordQuery = 'keyword=' + keyword + '&';
+		if (eventType) eventTypeQuery = 'classificationName=' + eventType + '&';
+		axios
+			.get(
+				`https://app.ticketmaster.com/discovery/v2/events.json?${keywordQuery}${eventTypeQuery}countryCode=GB&city=manchester&apikey=MPEjJbqNGhQLZI0ouzQ1GCmBLYxGHsqB`
+			)
+			.then(res => {
+				if (!res.data.page.totalElements)
+					this.setState({ events: [], isLoading: false });
+				else
+					this.setState(
+						{
+							events: util(res.data._embedded.events),
+							isLoading: false,
+							currentEventType: eventType
+						},
+						() => {
+							localStorage.setItem(
+								'currentEventType',
+								JSON.stringify(this.state.currentEventType)
+							);
+						}
+					);
+				if (callback) callback();
+			});
+	};
 
-  handleSearch = term => {
-    this.fetchEvents(term, '');
-  };
+	handleSearch = term => {
+		this.fetchEvents(term, this.state.currentEventType);
+	};
 
-  startLoading = () => {
-    this.setState({ isLoading: true });
-  };
+	startLoading = () => {
+		this.setState({ isLoading: true });
+	};
 }
 
 export default List;
